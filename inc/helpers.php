5917ca4a1de1d14ad3051ca9dcf718b7686b8b0a
Cuong Vuong <cuongtobi29@gmail.com>
2022-04-22 17:17:20 +0700

    Update helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -2,7 +2,6 @@
 function sswps_pagination($current, $total, $wrapper_class) {
 	if ($total > 1) {
 		echo '<div class="' . $wrapper_class . '">';
-
 		echo paginate_links(array(
 			'base' => get_pagenum_link(1) . '%_%',
 			'format' => 'page/%#%',
@@ -11,9 +10,7 @@ function sswps_pagination($current, $total, $wrapper_class) {
 			'prev_text' => '«',
 			'next_text' => '»',
 		));
-
 		echo '</div>';
 	}
-
 	wp_reset_postdata();
 }
