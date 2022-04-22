57417ada9b97a74c4abf4544cfb5a16b4ac45774
cuongtobi <cuongtobi29@gmail.com>
2022-04-22 14:57:04 +0700

    :hammer: Implement archive template

--- archive.php
+++ archive.php
@@ -1,2 +1,25 @@
 <?php
-// Archive template
+get_header();
+?>
+
+<?php the_archive_title('<h1>', '</h1>'); ?>
+
+<section>
+    <ul>
+	<?php
+	    if (have_posts()) {
+		while (have_posts()) {
+		    the_post();
+		    get_template_part('template-parts/content', get_post_type());
+		}
+	    } else {
+	        get_template_part('template-parts/content-none', get_post_type());
+	    }
+	?>
+    </ul>
+
+    <?php the_posts_pagination(); ?>
+</section>
+
+<?php
+get_footer();
